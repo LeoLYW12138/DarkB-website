fbfd3e1ff6184fffa30ff0922458cad08952b387
LeoLYW12138 <leo62227@gmail.com>
2020-05-31 15:07:38 +0800

    updated searchBar with keyup focus

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -1,24 +1,43 @@
 <template>
   <div>
-    <form class="flex align-center flex-1 bg-gray-300 rounded-full p-1 lg:pr-5 lg:pl-3">
-      <img class="inline fill-current" src="~/assets/images/icons/search.svg" alt="Search" />
-      <input
-        id="searchBar"
-        class="bg-gray-300"
-        type="text"
-        name="searchBar"
-        size="10"
-        placeholder="Search (Press '/' to focus)"
-      />
-    </form>
+    <search-focus @keyup="focusSearch"></search-focus>
+    <div class="flex flex-grow">
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 pl-1 pt-1 flex items-center">
+          <img
+            class="fill-current text-gray-500"
+            src="~/assets/images/icons/search.svg"
+            alt="Search"
+          />
+        </div>
+        <input
+          ref="search"
+          type="text"
+          label="Search"
+          placeholder="Search (Press &quot;/&quot; to focus)"
+          class="bg-gray-100 border border-gray-500 rounded-full px-4 pl-10 py-1 outline-none focus:border-blue-500"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        
+import searchFocus from '@/components/searchFocus.vue';
+export default {
+  components: {
+    'search-focus': searchFocus,
+  },
+  methods: {
+    focusSearch(e) {
+      if (e.key === '/') {
+        this.$refs.search.focus();
+      }
     }
+  },
+}
 </script>
+
 
 <style scoped>
 </style>
---
+++ components/searchFocus.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+    export default {
+        mounted () {
+          this.handler = event => {
+            this.$emit('keyup', event);
+          };
+          window.addEventListener('keyup', this.handler)
+        },
+        beforeDestroy () {
+          window.removeEventListener('keyup', this.handler);
+        },
+    }
+</script>
